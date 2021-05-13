3788f43b5cfe2222a1be63c8cb33cc5bc6392faf
CodingKid124 <sebastianweb231@gmail.com>
2021-05-13 20:37:04 +1200

    implement jquery

--- extension_resources/extension.js
+++ extension_resources/extension.js
@@ -1,13 +1,3 @@
-var html = document.createElement('div');
-    html.innerHTML = '<button id="clickMe">Change styling to GDK</button>';
-document.body.appendChild(html);
-
-document.getElementById('clickMe').addEventListener('click', function() {
-    var x = document.getElementById('clickMe');
-    if (x.textContent == "Change styling to GDK") {
-        document.getElementById('clickMe').textContent="Change styling to Classic";
-    }
-    else {
-        document.getElementById('clickMe').textContent="Change styling to GDK";
-    }
-});
+window.onload = function() {
+    /*   Code goes here   */ 
+}
