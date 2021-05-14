b4f2715f3c242c1ebd44324cdcaadde0c0250245
Jlakshan <jlakshan@protonmail.com>
2021-05-14 13:12:24 +1200

    fixed icon and changed GDK to GTk
    mind your bloody spelling next time

--- extension_resources/popup_extension.js
+++ extension_resources/popup_extension.js
@@ -1,13 +1,13 @@
 var html = document.createElement('div');
-    html.innerHTML = '<button id="clickMe">Change styling to GDK</button>';
+    html.innerHTML = '<button id="clickMe">Change styling to GTK</button>';
 document.body.appendChild(html);
 
 document.getElementById('clickMe').addEventListener('click', function() {
     var x = document.getElementById('clickMe');
-    if (x.textContent == "Change styling to GDK") {
+    if (x.textContent == "Change styling to GTK") {
         document.getElementById('clickMe').textContent="Change styling to Classic";
     }
     else {
-        document.getElementById('clickMe').textContent="Change styling to GDK";
+        document.getElementById('clickMe').textContent="Change styling to GTK";
     }
 });
