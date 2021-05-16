87ce3d920f8bb90f97d9a91942e5fcbbf9b06928
CodingKid124 <sebastianweb231@gmail.com>
2021-05-16 22:06:06 +1200

    slider button and local storage works

--- extension_resources/popup_extension.js
+++ extension_resources/popup_extension.js
@@ -1,37 +1,18 @@
-var html = document.createElement('div');
-    html.innerHTML = '<button id="clickMe">Change styling to GTK</button>';
-document.body.appendChild(html);
-
-document.getElementById('clickMe').addEventListener('click', function() {
-    var x = document.getElementById('clickMe');
-    if (x.textContent == "Change styling to GTK") {
-        document.getElementById('clickMe').textContent="Change styling to Classic";
-    }
-    else {
-        document.getElementById('clickMe').textContent="Change styling to GTK";
-    }
-});
-
-document.getElementById('testButton').addEventListener('click', function() {
-    var x = document.getElementById('testButton');
-    x.textContent = "Test worked";
-});
-/*
 document.getElementById('stateOfExtensionCheckbox').addEventListener('click', function() {
     const checkbox = document.getElementById('stateOfExtensionCheckbox');
     console.log("Is the checkbox checked?");
     console.log(checkbox.checked);
-    if (checkbox.checked == "true") {
-        checkbox.checked = "false";
-        var stateOfCheckbox = "false";
+    if (checkbox.checked == true) {
+        checkbox.checked = true;
+        var stateOfCheckbox = true;
         localStorage.setItem("stateOfExtensionButtonCheckbox", stateOfCheckbox);
         console.log("State of checkbox in local storage:");
         var testingvar = localStorage.getItem("stateOfExtensionButtonCheckbox");
         console.log(testingvar);
     }
     else {
-        checkbox.checked = "true";
-        var stateOfCheckbox = "true";
+        checkbox.checked = false;
+        var stateOfCheckbox = false;
         localStorage.setItem("stateOfExtensionButtonCheckbox", stateOfCheckbox);
         console.log("State of checkbox in local storage:");
         var testingvar = localStorage.getItem("stateOfExtensionButtonCheckbox");
@@ -39,32 +20,21 @@ document.getElementById('stateOfExtensionCheckbox').addEventListener('click', fu
     }
     console.log(checkbox.checked);
 });
-*/
 
-document.getElementById('stateOfExtensionCheckbox').addEventListener('click', function() {
-    var checkboxState = document.getElementById("stateOfExtensionCheckbox").checked;
-    console.log("Is the checkbox checked?");
-    console.log(checkboxState);
-    if (checkboxState === "true") {
-        console.log("If statement says checkbox is checked");
-    } else {
-        console.log("If statement says checkbox is not checked");
-    }
-});
 
-/*
+
 window.onload = function() {
     var checkboxState = localStorage.getItem("stateOfExtensionButtonCheckbox");
-    checkboxStateSetter = document.getElementById('stateOfExtensionCheckbox');
-    if (checkboxState == "checked") {
-        checkboxStateSetter.checked = "true";
+    var checkboxStateSetter = document.getElementById('stateOfExtensionCheckbox');
+    if (checkboxState == "true") {
+        checkboxStateSetter.checked = true;
     }
     else {
-        checkboxStateSetter.checked = "false";
+        checkboxStateSetter.checked = false;
     }
     console.log("Set state of checkbox to: (onload)");
     console.log(checkboxStateSetter.checked);
 }
-*/
+
 
 
